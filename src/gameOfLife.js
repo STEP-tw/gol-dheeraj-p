1961d34525767ea93f29f6d7c3fa205ba32999c6
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2018-11-29 14:15:56 +0530

    Refactor nextGeneration() : change logic

--- src/gameOfLife.js
+++ src/gameOfLife.js
@@ -4,11 +4,14 @@ const IO = require('./game_io.js');
 const nextGeneration = function(currGeneration,bounds) {
   let isWithinBounds = lib.isWithin.bind(null, bounds.topLeft, bounds.bottomRight);
   currGeneration = currGeneration.filter(isWithinBounds);
+  currGeneration = currGeneration.map((cell) => [cell[0] - bounds.topLeft[0], cell[1] - bounds.topLeft[1]]);
   currGeneration = IO.parseInputs(currGeneration);
   let {height, width} = IO.parseBounds(bounds);
   let world = lib.createWorld(currGeneration, height, width);
   let nextGeneration = lib.evaluateNextGeneration(world);
-  return IO.reverseParseInputs(nextGeneration);
+  return nextGeneration.map((cell) => {
+    return [cell.row + bounds.topLeft[0], cell.col + bounds.topLeft[1]];
+  });
 }
 
 module.exports = { nextGeneration };
