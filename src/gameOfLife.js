bd5bd0734cd60fad426fabf45bf2cb691b072a0e
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2018-11-29 12:20:44 +0530

    Refactor nextGeneration() : Use destructuring

--- src/gameOfLife.js
+++ src/gameOfLife.js
@@ -5,8 +5,7 @@ const nextGeneration = function(currGeneration,bounds) {
   let isWithinBounds = lib.isWithin.bind(null, bounds.topLeft, bounds.bottomRight);
   currGeneration = currGeneration.filter(isWithinBounds);
   currGeneration = IO.parseInputs(currGeneration);
-  let height = IO.parseBounds(bounds).height;
-  let width = IO.parseBounds(bounds).width;
+  let {height, width} = IO.parseBounds(bounds);
   let world = lib.createWorld(currGeneration, height, width);
   let nextGeneration = lib.evaluateNextGeneration(world);
   return IO.reverseParseInputs(nextGeneration);
