5c9fb9fe9a01c417073731092daa0cb749d24a95
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2018-12-03 09:57:04 +0530

    Extract filterOutOfBoundCells() from nextGeneration()

--- src/gameOfLife.js
+++ src/gameOfLife.js
@@ -2,16 +2,21 @@ const lib = require('./lib.js');
 const IO = require('./game_io.js');
 
 const nextGeneration = function(currGeneration,bounds) {
-  let isWithinBounds = lib.isWithin.bind(null, bounds.topLeft, bounds.bottomRight);
-  currGeneration = currGeneration.filter(isWithinBounds);
+  currGeneration = filterOutOfBoundCells(currGeneration, bounds);
   currGeneration = currGeneration.map((cell) => [cell[0] - bounds.topLeft[0], cell[1] - bounds.topLeft[1]]);
   currGeneration = IO.parseInputs(currGeneration);
+
   let {height, width} = IO.parseBounds(bounds);
   let world = lib.createWorld(currGeneration, height, width);
   let nextGeneration = lib.evaluateNextGeneration(world);
+
   return nextGeneration.map((cell) => {
     return [cell.row + bounds.topLeft[0], cell.col + bounds.topLeft[1]];
   });
 }
 
+const filterOutOfBoundCells = function(cells, bounds){
+  let isWithinBounds = lib.isWithin.bind(null, bounds.topLeft, bounds.bottomRight);
+  return cells.filter(isWithinBounds);
+}
 module.exports = { nextGeneration };
